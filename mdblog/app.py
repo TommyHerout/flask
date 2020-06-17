dea7222f13bd7ec3dc8e2b848239cb75176a01ef
Tomas <tomas.herout@post.cz>
2020-06-17 17:37:09 +0000

    updated CSS of login and logout

--- mdblog/app.py
+++ mdblog/app.py
@@ -5,7 +5,7 @@ from flask import redirect
 from flask import url_for
 from flask import session
 from flask import g
-
+from flask import flash
 
 import sqlite3
 import os
@@ -18,7 +18,6 @@ flask_app.config.from_pyfile("/vagrant/configs/default.py")
 if "MDBLOG_CONFIG" in os.environ:
     flask_app.config.from_envvar("MDBLOG_CONFIG")
 
-
 @flask_app.route("/")
 def view_welcome_page():
     return render_template("welcome_page.jinja")
@@ -30,6 +29,7 @@ def view_about():
 @flask_app.route("/admin/")
 def view_admin():
     if "logged" not in session:
+        flash("You must be logged in", "alert-danger")
         return redirect(url_for("view_login"))
     return render_template("admin.jinja")
 
@@ -46,12 +46,13 @@ def add_article():
     db.execute("insert into articles (title, content) values (?, ?)",
             [request.form.get("title"), request.form.get("content")])
     db.commit()
+    flash("Article was saved", "alert-success")
     return redirect(url_for("view_articles"))
 
-@flask_app.route("/articles/<int:art_id>")
+@flask_app.route("/articles/<int:art_id>/")
 def view_article(art_id):
     db = get_db()
-    cur = db.execute("select * from articles where id=(?)", [art_id])
+    cur = db.execute("select * from articles where id=(?)",[art_id])
     article = cur.fetchone()
     if article:
         return render_template("article.jinja", article=article)
@@ -68,17 +69,20 @@ def login_user():
     if username == flask_app.config["USERNAME"] and \
             password == flask_app.config["PASSWORD"]:
         session["logged"] = True
+        flash("Login successful", "alert-success")
         return redirect(url_for("view_admin"))
     else:
+        flash("Invalid credentials", "alert-danger")
         return redirect(url_for("view_login"))
-    
+
 @flask_app.route("/logout/", methods=["POST"])
 def logout_user():
     session.pop("logged")
+    flash("Logout successful", "alert-success")
     return redirect(url_for("view_welcome_page"))
 
-## UTILS
 
+## UTILS
 def connect_db():
     rv = sqlite3.connect(flask_app.config["DATABASE"])
     rv.row_factory = sqlite3.Row
@@ -99,3 +103,4 @@ def init_db(app):
         db = get_db()
         with open("mdblog/schema.sql", "r") as fp:
             db.cursor().executescript(fp.read())
+        db.commit()
